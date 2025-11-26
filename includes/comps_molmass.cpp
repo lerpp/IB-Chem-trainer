0b5162c6656e6040231d319616f4753947274b6b
Allen <120138851+lerpp@users.noreply.github.com>
2025-11-26 12:47:21 -0500

    added new compounds

--- includes/comps_molmass.cpp
+++ includes/comps_molmass.cpp
@@ -17,11 +17,15 @@ vector<pair<pair<string, string>, double>> compounds_molmass = {
     {{"table salt", "NaCl"}, 58.44},
     {{"magnesium oxide", "MgO"}, 40.31},
     {{"sulfur dioxide", "SO2"}, 64.07},
+    {{"sulfuric acid", "H2SO4"}, 98.09},
+    {{"calcium carbonate", "CaCO3"}, 100.09},
+    {{"ammonium nitrate", "NH4NO3"}, 80.06},
     {{"ethane", "C2H6"}, 30.08},
     {{"ethanol", "C2H5OH"}, 46.08},
     {{"glucose", "C6H12O6"}, 180.18},
     {{"acetic acid", "CH3COOH"}, 60.06},
-    {{"propane", "C3H8"}, 44.11}
+    {{"propane", "C3H8"}, 44.11},
+    {{"benzene", "C6H6"}, 78.12}
 };
 
 void find_comp_molmass() {
