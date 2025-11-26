5425b9b092bd171a06a142bfe9a79f3379b22ed3
Allen <120138851+lerpp@users.noreply.github.com>
2025-11-25 20:48:03 -0500

    stopped using bits/stdc++.h and namespace std

--- includes/molar_mass.cpp
+++ includes/molar_mass.cpp
@@ -1,8 +1,9 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 #include "molar_mass.h"
 #include "input_handling.h"
 
-using namespace std;
+using std::cin, std::cout, std::string;
 
 void calcMolMass(){
     double n = roundDouble(rand() % 1000 / 100.0, 3);
