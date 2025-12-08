12ef2d6f205ac5b9b682fcf0bfe63eec09b26ee6
Allen <120138851+lerpp@users.noreply.github.com>
2025-12-08 09:28:00 -0500

    added hybridization

--- includes/vsepr.cpp
+++ includes/vsepr.cpp
@@ -7,36 +7,37 @@
 
 using std::cin, std::cout, std::string, std::vector, std::pair, std::getline, std::ws;
 
-vector<pair<string, pair<pair<string, string>, pair<string, string>>>> compounds_vsepr = {
-    {"H2S",      {{"tetrahedral",                "bent"}, {"<<109.5",   "polar"}}},
-    {"CF4",      {{"tetrahedral",         "tetrahedral"}, {"109.5", "non-polar"}}},
-    {"HCN",      {{"linear",                   "linear"}, {"180",       "polar"}}},
-    {"NF3",      {{"tetrahedral",    "trigonal pyramid"}, {"<109.5",    "polar"}}},
-    {"BCl3",     {{"trigonal planar", "trigonal planar"}, {"120",   "non-polar"}}},
-    {"NH2Cl",    {{"tetrahedral",    "trigonal pyramid"}, {"<109.5",    "polar"}}},
-    {"OF2",      {{"tetrahedral",                "bent"}, {"<<109.5",   "polar"}}},
-    {"PH3",      {{"tetrahedral",    "trigonal pyramid"}, {"<109.5",    "polar"}}},
-    {"BeCl2",    {{"linear",                   "linear"}, {"180",   "non-polar"}}},
-    {"XeF2",     {{"trigonal bipyramid",       "linear"}, {"180",   "non-polar"}}},
-    {"XeO4",     {{"tetrahedral",         "tetrahedral"}, {"109.5", "non-polar"}}},
-    {"ClBr3",    {{"trigonal bipyramid",      "t-shape"}, {"<90",       "polar"}}},
-    {"TeF6",     {{"octahedral",           "octahedral"}, {"90",    "non-polar"}}},
-    {"IF3",      {{"trigonal bipyramid",      "t-shape"}, {"<90",       "polar"}}},
-    {"[PCl6]-",  {{"octahedral",           "octahedral"}, {"90",    "non-polar"}}},
-    {"[IO4]-",   {{"tetrahedral",         "tetrahedral"}, {"109.5", "non-polar"}}},
-    {"[BrF4]-",  {{"octahedral",        "square planar"}, {"90",    "non-polar"}}},
-    {"[PCl4]-",  {{"trigonal bipyramid",       "seesaw"}, {"<90, <120", "polar"}}},
-    {"[FCl2]+",  {{"tetrahedral",                "bent"}, {"<<109.5",   "polar"}}},
-    {"[ClO3]-",  {{"tetrahedral",    "trigonal pyramid"}, {"<109.5",    "polar"}}},
-    {"[NO2]+",   {{"linear",                   "linear"}, {"180",   "non-polar"}}},
-    {"[NO2]-",   {{"trigonal planar",            "bent"}, {"<120",      "polar"}}},
-    {"[ClF2]+",  {{"tetrahedral",                "bent"}, {"<<109.5",   "polar"}}},
-    {"[SnCl3]-", {{"tetrahedral",    "trigonal pyramid"}, {"<109.5",    "polar"}}}
+vector<pair<string, pair<pair<pair<string, string>, string>, pair<string, string>>>> compounds_vsepr = {
+    {"H2S",{{{"tetrahedral","bent"}, "sp3"}, {"<<109.5","polar"}}},
+    {"CF4",{{{"tetrahedral","tetrahedral"}, "sp3"}, {"109.5", "non-polar"}}},
+    {"HCN",{{{"linear","linear"}, "sp"}, {"180","polar"}}},
+    {"NF3",{{{"tetrahedral","trigonal pyramid"}, "sp3"}, {"<109.5","polar"}}},
+    {"BCl3",{{{"trigonal planar", "trigonal planar"}, "sp2"}, {"120","non-polar"}}},
+    {"NH2Cl",{{{"tetrahedral","trigonal pyramid"}, "sp3"}, {"<109.5","polar"}}},
+    {"OF2",{{{"tetrahedral","bent"}, "sp3"}, {"<<109.5","polar"}}},
+    {"PH3",{{{"tetrahedral","trigonal pyramid"}, "sp3"}, {"<109.5","polar"}}},
+    {"BeCl2",{{{"linear","linear"}, "sp"}, {"180","non-polar"}}},
+    {"XeF2",{{{"trigonal bipyramid","linear"}, "n/a"}, {"180","non-polar"}}},
+    {"XeO4",{{{"tetrahedral","tetrahedral"}, "sp3"}, {"109.5", "non-polar"}}},
+    {"ClBr3",{{{"trigonal bipyramid","t-shape"}, "n/a"}, {"<90","polar"}}},
+    {"TeF6",{{{"octahedral","octahedral"}, "n/a"}, {"90","non-polar"}}},
+    {"IF3",{{{"trigonal bipyramid","t-shape"}, "n/a"}, {"<90","polar"}}},
+    {"[PCl6]-",{{{"octahedral","octahedral"}, "n/a"}, {"90","non-polar"}}},
+    {"[IO4]-",{{{"tetrahedral","tetrahedral"}, "sp3"}, {"109.5", "non-polar"}}},
+    {"[BrF4]-",{{{"octahedral","square planar"}, "n/a"}, {"90","non-polar"}}},
+    {"[PCl4]-",{{{"trigonal bipyramid","seesaw"}, "n/a"}, {"<90, <120", "polar"}}},
+    {"[FCl2]+",{{{"tetrahedral","bent"}, "sp3"}, {"<<109.5","polar"}}},
+    {"[ClO3]-",{{{"tetrahedral","trigonal pyramid"}, "sp3"}, {"<109.5","polar"}}},
+    {"[NO2]+",{{{"linear","linear"}, "sp"}, {"180","non-polar"}}},
+    {"[NO2]-",{{{"trigonal planar","bent"}, "sp2"}, {"<120","polar"}}},
+    {"[ClF2]+",{{{"tetrahedral","bent"}, "sp3"}, {"<<109.5","polar"}}},
+    {"[SnCl3]-", {{{"tetrahedral","trigonal pyramid"}, "sp3"}, {"<109.5","polar"}}}
 };
 
 void find_vsepr() {
     auto [compound, data] = compounds_vsepr[rand() % compounds_vsepr.size()];
-    auto [geometries, properties] = data;
+    auto [shape, properties] = data;
+    auto [geometries, hybridization] = shape;
     auto [elec_geo, molec_geo] = geometries;
     auto [bond_angles, polarity] = properties;
     cout << "VSEPR: " << compound << "\n";
@@ -49,6 +50,12 @@ void find_vsepr() {
     getline(cin >> ws, ans);
     checkAnswerS(ans, molec_geo);
     next();
+    if (hybridization != "n/a") {
+        cout << "State the hybridization of " << compound << '\n';
+        getline(cin >> ws, ans);
+        checkAnswerS(ans, hybridization);
+        next();
+    }
     cout << "State the bond angles of " << compound << " in ascending order\n(Use < and << for less than and much less than)\n";
     getline(cin >> ws, ans);
     checkAnswerS(ans, bond_angles);
