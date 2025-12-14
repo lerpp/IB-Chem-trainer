fe8c9e41d31de1b3820099e14b141bd135376e91
Allen <120138851+lerpp@users.noreply.github.com>
2025-12-14 08:48:45 -0500

    added focused unit selection

--- main.cpp
+++ main.cpp
@@ -23,9 +23,9 @@
 #include "includes/intermol_forces.h"
 #include "includes/polymers.h"
 
-using std::vector, std::cin, std::getline, std::ws, std::string;
+using std::vector, std::cin, std::cout, std::getline, std::ws, std::string, std::pair;
 
-const vector questions = {
+const vector all_questions = {
     pickCueCard,
     pickFiltration,
     pickState,
@@ -52,11 +52,55 @@ const vector questions = {
     condensation_polymer
 };
 
+const vector structure1 = {
+    pickFiltration,
+    pickState,
+    calcAr,
+    calcMolMass,
+    calcFreq,
+    emissionSpectrum,
+    calcIE,
+    elecConfig,
+    find_comp_molmass,
+    percent_comp,
+    emp_formula,
+    mol_formula,
+    find_hydrate,
+    cnv,
+    cvcv,
+    comb_gas_law,
+    ideal_gas_law
+};
+
+const vector structure2 = {
+    draw_lewis,
+    find_vsepr,
+    identify_imfs,
+    compare_bp,
+    addition_polymer,
+    condensation_polymer
+};
+
+const vector cuecards = {
+    pickCueCard
+};
+
+const vector<pair<vector<void(*)()>, string>> units = {
+    {cuecards, "Cue cards"},
+    {structure1, "Structure 1"},
+    {structure2, "Structure 2"},
+    {all_questions, "Everything"}
+};
+
 int main() {
     srand(time(0));
+    cout << "Select your desired topic\n";
+    for (int i = 1; i <= units.size(); i++) cout << units[i - 1].second << " (" << i << ")\n";
+    int unit; cin >> unit;
+    vector activity = units[unit - 1].first;
     while(true) {
-        int activity = rand() % questions.size();
-        questions[activity]();
+        int task = rand() % activity.size();
+        activity[task]();
         next();
     }
     return 0;
