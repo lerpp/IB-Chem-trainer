b1b80efd93624667e02f11faf90996f04ec4e9bb
lerpp <allen.li103@gmail.com>
2026-01-13 08:51:49 -0500

    added file to cmake fixed sign for answer on calorimetry activity

--- includes/calorimetry.cpp
+++ includes/calorimetry.cpp
@@ -13,7 +13,7 @@ void calorimetry() {
     double m = rand() % 1000 / 10.0;
     double t = rand() % 1000 / 100.0 * pow(-1, rand() % 2 + 1);
     double q = roundDouble(m * c_water * t, 3);
-    double h = roundDouble(q / scalar, 3);
+    double h = roundDouble(-q / scalar, 3);
     cout << "A reaction occurs inside a perfectly sealed and insulated calorimeter containing " << m << " mL of water, changing the temperature of the water by " << t << " K\nThere are " << scalar << " mol of the compound of interest\nWhat is the molar enthalpy of the reaction?\n";
     double ans; cin >> ans;
     checkAnswerD(ans, h);
